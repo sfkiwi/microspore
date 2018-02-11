22fa9d148577a6ce395acf94744f5687f6785870
Mike Sutherland <mjfsutherland@gmail.com>
2018-02-10 17:40:28 -0800

    fixing route handlers

--- services/events/analytics/analytics.js
+++ services/events/analytics/analytics.js
@@ -5,19 +5,24 @@ import logs from './routes/logs';
 
 const app = new Koa();
 
+const base = new Router();
+
 const router = new Router({
   prefix: '/api'
 });
 
-router
+base
   .get('/', async ctx => {
-    ctx.body = {hello: 'World'};
+    ctx.body = { hello: 'World' };
     console.log('Hello World');
-  })
+  });
+
+router
   .use('/stats', stats.routes())
   .use('/logs', logs.routes());
 
 app
+  .use(base.routes())
   .use(router.routes())
   .use(router.allowedMethods())
   .on('error', err => {
